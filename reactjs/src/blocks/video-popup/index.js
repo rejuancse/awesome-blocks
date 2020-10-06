c4cf1f2e0040fca254f52d65b5e41f0f6d4772ca
Rejuan Ahamed <rejuancse@gmail.com>
2020-10-06 23:01:07 +0600

    video popup block script change

--- reactjs/src/blocks/video-popup/index.js
+++ reactjs/src/blocks/video-popup/index.js
@@ -30,52 +30,6 @@ registerBlockType( 'awesome-blocks/awesomevideopopup', {
         //Destructuring the images array attribute
 		const {images = [], columns, padding, gallery_style} = attributes;
 
-        // This removes an image from the gallery
-        const removeImage = (removeImage) => {
-			//filter the images
-			const newImages = images.filter( (image) => {
-				if(image.id != removeImage.id) {
-					return image;
-				}
-			});
-			// Saves the new state
-			setAttributes({
-				images:newImages,
-			})
-		}
-
-
-		const GalleryStyleControl = withState({
-            gallery_style: gallery_style,
-        })(({ gallery_style, setState }) => (
-            <SelectControl
-                label="Select Style"
-                value={gallery_style}
-                options={[
-                    { label: 'Style One', value: 'style1' },
-                    { label: 'Style Two', value: 'style2' },
-                ]}
-                onChange={(value) => { setAttributes({ gallery_style: value }) }}
-            />
-        ));
-	
-		// Select column
-        const GallerySelectColumn = withState({
-            column: columns,
-        })(({ column, setState }) => (
-            <SelectControl
-                label="Select Column"
-                value={column}
-                options={[
-                    { label: 'One Column', value: '1' },
-                    { label: 'Two Column', value: '2' },
-                    { label: 'Three Column', value: '3' },
-                    { label: 'Four Column', value: '4' },
-                ]}
-                onChange={(value) => { setAttributes({ columns: value }) }}
-            />
-		));
-
 		// Gallery Padding 
         const GalleryContentControl = withState( {
             padding: padding,
@@ -86,67 +40,15 @@ registerBlockType( 'awesome-blocks/awesomevideopopup', {
                 onChange={(value) => { setAttributes({ padding: value }) }}
             />
         ) );
-		
-		let counts = 0;
-		let output = '';
-
-        //Displays the images
-        const displayImages = (images) => {
-			return (
-                images.map( (image) => {
-
-					const paddingStyle = {
-						padding: padding
-					}
-
-					if(gallery_style == 'style1') {
-						return (
-							<div className={`col-${columns} gallery-item`} style={paddingStyle}>
-								<img className='gallery-item' src={image.url} key={ images.id } />
-								<div className='remove-item' onClick={() => removeImage(image)}><span class="dashicons dashicons-trash"></span></div>
-							</div>
-						)
-					}else {
-						
-						output = (counts == 0) ? (
-							<div className={`col-md-6 full-content style2-wrap`} style={paddingStyle}>
-								<div className="gallery-item">
-									<img className='gallery-item' src={image.url} key={ images.id } />
-									<div className='remove-item' onClick={() => removeImage(image)}><span class="dashicons dashicons-trash"></span></div>
-								</div>
-							</div> 
-						) : (					
-							<div className={`col-md-3 gallery-style2`} style={paddingStyle}>
-								<div className="gallery-item">
-									<img className='gallery-item' src={image.url} key={ images.id } />
-									<div className='remove-item' onClick={() => removeImage(image)}><span class="dashicons dashicons-trash"></span></div>
-								</div>
-							</div> 
-						);
-
-						counts++;
-						return output;
-
-					}
-                    
-                })
-            )
-        }
 
         //JSX to return
         return (
             <div>
 				<InspectorControls key="inspector">
-                    <PanelBody title={__('Select Gallery Column')}>
-						<GalleryStyleControl />
-                        <GallerySelectColumn />
+                    <PanelBody title={__('Video Popup')}>
 						<GalleryContentControl />
                     </PanelBody>
                 </InspectorControls>
-
-                <div className="gallery-grid">
-                    {displayImages(images)}
-                </div>
 
                 <br/>
                 <MediaUpload
@@ -166,10 +68,7 @@ registerBlockType( 'awesome-blocks/awesomevideopopup', {
 	
 	save({attributes}) {
 		//Destructuring the images array attribute
-		const { images = [], columns, padding, gallery_style } = attributes;
-
-		let counts = 0;
-		let output = '';
+		const { images = [], columns, padding } = attributes;
 	
 		// Displays the images
 		const displayImages = (images) => {
@@ -180,7 +79,7 @@ registerBlockType( 'awesome-blocks/awesomevideopopup', {
 						padding: padding
 					}
 
-					if(gallery_style == 'style1') {
+					
 						return (
 							<div className={`col-${columns} gallery-item`} style={paddingStyle}>
 								<a href={image.url} className="cloud-zoom">
@@ -195,28 +94,9 @@ registerBlockType( 'awesome-blocks/awesomevideopopup', {
 								</a>
 							</div>
 						)
-					}else {
-						output = (counts == 0) ? (
-							<div className={`col-md-6 full-content style2-wrap`} style={paddingStyle}>
-								<div className="gallery-item">
-								<a href={image.url} className="cloud-zoom">
-									<img className='gallery-item' src={image.url} key={ images.id } />
-								</a>
-								</div>
-							</div> 
-						) : (					
-							<div className={`col-md-3 gallery-style2`} style={paddingStyle}>
-								<div className="gallery-item">
-								<a href={image.url} className="cloud-zoom">
-									<img className='gallery-item' src={image.url} key={ images.id } />
-								</a>
-								</div>
-							</div> 
-						);
-
-						counts++;
-						return output;
-					}
+					
+					
+					
 					
 				})
 			)
@@ -226,6 +106,7 @@ registerBlockType( 'awesome-blocks/awesomevideopopup', {
 		return (
 			<div  data-total-slides={images.length}>{ displayImages(images) }</div>	
 		);
+
 	},
 	
 } );
