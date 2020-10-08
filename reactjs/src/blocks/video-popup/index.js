af8cee858ab48c25984282ef6811626c343f81e1
Rejuan Ahamed <rejuancse@gmail.com>
2020-10-08 21:30:17 +0600

    Video script add

--- reactjs/src/blocks/video-popup/index.js
+++ reactjs/src/blocks/video-popup/index.js
@@ -10,101 +10,37 @@ const { PanelBody, Button, Spinner, SelectControl, RangeControl, ColorPalette, T
 
 
 //  Import CSS.
-import './editor.scss';
-import './style.scss'
+// import './editor.scss';
+// import './style.scss'
 
 registerBlockType( 'awesome-blocks/awesomevideopopup', {
 	title: __( 'Video Popup' ),
 	icon: 'format-video',
 	category: 'awesome-blocks',
 	attributes: { //Attributes
-		images : { type: 'array'},
-		columns: { type: 'string', default: '3' }, 
-		padding: { type: 'string', default: '15px' }, 
-		gallery_style: { type: 'string', default: 'style1' }, 
+		// images : { type: 'array'},
 	},
 
  
 	edit({ attributes, className, setAttributes }) {
-
-        //Destructuring the images array attribute
-		const {images = [], columns, padding, gallery_style} = attributes;
-
-		// Gallery Padding 
-        const GalleryContentControl = withState( {
-            padding: padding,
-        } )( ( { padding, setState } ) => ( 
-            <TextControl
-                label="Padding"
-                value={ padding }
-                onChange={(value) => { setAttributes({ padding: value }) }}
-            />
-        ) );
-
         //JSX to return
         return (
             <div>
-				<InspectorControls key="inspector">
-                    <PanelBody title={__('Video Popup')}>
-						<GalleryContentControl />
-                    </PanelBody>
-                </InspectorControls>
-
-                <br/>
-                <MediaUpload
-					onSelect={(media) => {setAttributes({images: [...images, ...media]});}}
-					type="image"
-					multiple={true}
-					value={images}
-					render={({open}) => (
-						<Button className="select-images-button is-button is-default is-large" onClick={open}>
-							Add images
-						</Button>
-					)}
-				/>
+				Video Popup blocks
             </div>
         );
 	},
 	
 	save({attributes}) {
 		//Destructuring the images array attribute
-		const { images = [], columns, padding } = attributes;
+		const { } = attributes;
 	
-		// Displays the images
-		const displayImages = (images) => {
-			return (
-				images.map( (image,index) => {
-
-					const paddingStyle = {
-						padding: padding
-					}
-
-					
-						return (
-							<div className={`col-${columns} gallery-item`} style={paddingStyle}>
-								<a href={image.url} className="cloud-zoom">
-									<img
-										className='gallery-item'
-										key={images.id}
-										src={image.url}
-										data-slide-no={index}
-										data-caption={image.caption[0]}
-										alt={image.alt}
-									/>
-								</a>
-							</div>
-						)
-					
-					
-					
-					
-				})
-			)
-		}
-	
+		
 		//JSX to return
 		return (
-			<div  data-total-slides={images.length}>{ displayImages(images) }</div>	
+			<div>
+				Save view
+			</div>	
 		);
 
 	},
